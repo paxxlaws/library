c1ccd9ebad81a2ebc6e7faef5406d4b090dec554
paxxlaws <paxschwarz@gmail.com>
2019-11-02 21:29:40 -0700

    test commit from CS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,25 @@
-import React, { useState } from 'react';
-import { Jumbotron, H1, P } from '@bootstrap-styled/v4';
+import React, { useState } from "react";
+import { Jumbotron, H1, P } from "@bootstrap-styled/v4";
 import Game from "../components/Game.js";
-import { games } from "../helpers/Games.js";
+import { games } from "../helpers/games.js";
 
 export default function Home() {
-
   const gameList = () => {
     var list = [];
-    
+
     for (var key in games) {
-      list.push(<Game id={games[key].id} title={games[key].name} year={games[key].year} desc={games[key].desc} url={games[key].url}/>
+      list.push(
+        <Game
+          id={games[key].id}
+          title={games[key].name}
+          year={games[key].year}
+          desc={games[key].desc}
+          url={games[key].url}
+        />
       );
     }
     return list;
-  }
+  };
 
   return (
     <React.Fragment>
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,26 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Switch, Route } from "react-router-dom";
-import { findGame } from "../helpers/Games.js";
-import { 
-    H1,
-    Jumbotron,
-    P
-  } from '@bootstrap-styled/v4';
+import { findGame } from "../helpers/games.js";
+import { H1, Jumbotron, P } from "@bootstrap-styled/v4";
 import TopNav from "../components/TopNav.js";
 import Detail from "./Detail.js";
-import SectionList from '../components/SectionList.js';
+import SectionList from "../components/SectionList.js";
 
 export default function Landing(props) {
   var game = findGame(props.match.params.name);
 
   return (
     <React.Fragment>
-      <TopNav title={game.name}/>
+      <TopNav title={game.name} />
       <Switch>
         <Route exact path={`${props.match.path}`} component={SectionList} />
-        <Route path={'/game/:name/:section'} component={Detail} />
+        <Route path={"/game/:name/:section"} component={Detail} />
       </Switch>
-      <TopNav title={game.name}/>
+      <TopNav title={game.name} />
     </React.Fragment>
   );
 }
